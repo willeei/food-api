624c0a149fa597b605441e910cbef8f032665510
Williams Gomes <williamsgomess@outlook.com>
2020-02-11 17:56:22 -0300

    03.06-mapeando-o-id-da-entidade-para-autoincremento

--- src/main/java/com/wwwgomes/food/domain/model/Cozinha.java
+++ src/main/java/com/wwwgomes/food/domain/model/Cozinha.java
@@ -1,18 +1,16 @@
 package com.wwwgomes.food.domain.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "tab_cozinhas")
 public class Cozinha {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
-	@Column(name = "nom_cozinha")
 	private String nome;
 
 	public Long getId() {
