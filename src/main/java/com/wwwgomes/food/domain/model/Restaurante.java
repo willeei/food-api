f82b5fc19460ced3883c303537433adbfaad67b9
Williams Gomes <williamsgomess@outlook.com>
2020-04-28 18:43:09 -0300

    03.18-a-anotacao-joincolumn

--- src/main/java/com/wwwgomes/food/domain/model/Restaurante.java
+++ src/main/java/com/wwwgomes/food/domain/model/Restaurante.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
@@ -23,11 +24,12 @@ public class Restaurante {
 	private Long id;
 	
 	private String nome;
-
+	
 	@Column(name = "taxa_frete")
 	private BigDecimal taxaFrete;
 	
 	@ManyToOne
+	@JoinColumn(name = "cozinha_id")
 	private Cozinha cozinha;
 
 }
