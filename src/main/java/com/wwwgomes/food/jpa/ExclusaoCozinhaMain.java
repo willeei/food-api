fce9f4d1f0cb49a533f70a21ed3dd1c06b026c48
Williams Gomes <williamsgomess@outlook.com>
2020-04-28 17:41:29 -0300

    03.12-excluindo-um-objeto-do-banco-de-dados

---
+++ src/main/java/com/wwwgomes/food/jpa/ExclusaoCozinhaMain.java
@@ -0,0 +1,25 @@
+package com.wwwgomes.food.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.wwwgomes.food.FoodApiApplication;
+import com.wwwgomes.food.domain.model.Cozinha;
+
+public class ExclusaoCozinhaMain {
+	
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(FoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		Cozinha cozinha = new Cozinha();
+		cozinha.setId(1L);
+		
+		cadastroCozinha.remover(cozinha);
+	}
+
+}
