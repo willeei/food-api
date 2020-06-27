356878917f1e19350df9a8aa55a903019f1499b7
Williams Gomes <williamsgomess@outlook.com>
2020-06-27 13:55:53 -0300

    Modulo 04
    04.10 - Modelando e requisitando um collection resource com get
    04.11 - Desafio collection resource de estados
    04.13 - Implementando content negociation para retornar json ou xml
    04.14 - Consultando singleton resource com get e pathvariableModulo 04
    
    04.10 - Modelando e requisitando um collection resource com get
    04.11 - Desafio collection resource de estados
    04.13 - Implementando content negociation para retornar json ou xml
    04.14 - Consultando singleton resource com get e pathvariableModulo 04
    
    04.10 - Modelando e requisitando um collection resource com get
    04.11 - Desafio collection resource de estados
    04.13 - Implementando content negociation para retornar json ou xml
    04.14 - Consultando singleton resource com get e pathvariableModulo 04
    
    04.10 - Modelando e requisitando um collection resource com get
    04.11 - Desafio collection resource de estados
    04.13 - Implementando content negociation para retornar json ou xml
    04.14 - Consultando singleton resource com get e pathvariable

---
+++ src/main/java/com/wwwgomes/food/api/EstadoController.java
@@ -0,0 +1,25 @@
+package com.wwwgomes.food.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wwwgomes.food.domain.model.Estado;
+import com.wwwgomes.food.domain.repository.EstadoRepository;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@GetMapping
+	public List<Estado> listar() {
+		return estadoRepository.listar();
+	}
+	
+}
